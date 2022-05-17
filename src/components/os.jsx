67ce2e9c651296a8915e5a1bfad1dffe58d430b7
AlishaMohasin <alishamohasin85@gmail.com>
2022-05-18 00:43:43 +0530

    first commit

---
+++ src/components/os.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Os=()=>{
+    return <div><h1>Mobile Operating System</h1>
+    <div class="a">
+         <li>Android</li>
+         <li>Blackberry</li>
+         <li>iphone</li>
+         <li>Windows phone</li></div>
+        <div class="b"> <h1>Mobile Manufacturers</h1>
+         <li>Samsung</li>
+         <li>HTC</li>
+         <li>Micromax</li>
+         <li>Apple</li></div>
+    </div>
+}
+export default Os;
