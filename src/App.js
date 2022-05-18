01b3d94918862e190108e653eefe0baf48f183c4
AlishaMohasin <alishamohasin85@gmail.com>
2022-05-18 16:10:45 +0530

    changed

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Os from './components/os';
 
 function App() {
   return (
-    <div className="App">
+    <div >
       <Os/>
     </div>
   );
--- src/components/os.jsx
+++ src/components/os.jsx
@@ -2,16 +2,16 @@ import React from "react";
 
 const Os=()=>{
     return <div><h1>Mobile Operating System</h1>
-    <div class="a">
+    <ul>
          <li>Android</li>
          <li>Blackberry</li>
          <li>iphone</li>
-         <li>Windows phone</li></div>
-        <div class="b"> <h1>Mobile Manufacturers</h1>
+         <li>Windows phone</li></ul>
+       <ul> <h1>Mobile Manufacturers</h1>
          <li>Samsung</li>
          <li>HTC</li>
          <li>Micromax</li>
-         <li>Apple</li></div>
+         <li>Apple</li></ul>
     </div>
 }
 export default Os;
